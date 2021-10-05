8ec40c5f1567a89b4e3f05099159098f151285fa
gilimaman@outlook.com <gilimaman@outlook.com>
2021-10-05 03:22:08 +0300

    initial code + Welcome functions

---
+++ Targil0/Program9627.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome9627();
+            Welcome5223();
+            Console.ReadKey();
+        }
+
+        private  static void Welcome9627()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+
+        static partial void Welcome5223();
+    }
+}
