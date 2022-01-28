a446b0bdf5a82c68e61231a909529ca4d29ce5df
TamarShterenfeld <91895235+TamarShterenfeld@users.noreply.github.com>
2022-01-28 10:42:08 +0200

    xml
    xml

---
+++ dotNet5782_9627_5223/DalXml/CheckIdExistence.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+namespace DalXml
+{
+    public partial class DalXml
+    {
+        /// <summary>
+        /// check if the id exists in the baseStationsList
+        /// </summary>
+        /// <param name="baseStationId">the id for checking</param>
+        static void CheckExistenceOfBaseStation(int baseStationId)
+        {       
+            
+        }
+
+        /// <summary>
+        /// check if the id doesn't exist in the BaseStationsList
+        /// </summary>
+        /// <param name="baseStationId">the id for checking</param>
+        static void CheckNotExistenceOfBaseStation(int baseStationId)
+        {
+        }
+
+        /// <summary>
+        /// check if the id exists in the CustomersList
+        /// </summary>
+        /// <param name="customerId">the id for checking</param>
+        static void CheckExistenceOfCustomer(string customerId)
+        {
+            
+        }
+
+        /// <summary>
+        /// check if the id doesn't exist in the CustomersList
+        /// </summary>
+        /// <param name="customerId">the id for checking</param>
+        static void CheckNotExistenceOfCustomer(string customerId)
+        {
+            
+        }
+
+        /// <summary>
+        /// check if the id exists in the DronesList
+        /// </summary>
+        /// <param name="droneId">the id for checking</param>
+        static void CheckExistenceOfDrone(int droneId)
+        {
+
+        }
+
+        /// <summary>
+        /// check if the id doesn't exist in the DronesList
+        /// </summary>
+        /// <param name="droneId">the id for checking</param>
+        static void CheckNotExistenceOfDrone(int droneId)
+        {
+          
+        }
+
+        /// <summary>
+        /// check if the id exists in the ParcelsList
+        /// </summary>
+        /// <param name="parcelId">the id for checking</param>
+        static void CheckExistenceOfParcel(int parcelId)
+        {
+            
+        }
+
+        /// <summary>
+        /// check if the id doesn't exist in the ParcelsList
+        /// </summary>
+        /// <param name="parcelId">the id for checking</param>
+        static void CheckNotExistenceOfParcel(int parcelId)
+        {
+            
+        }
+
+        /// <summary>
+        /// check if the id exists in the DronesChargeList
+        /// </summary>
+        /// <param name="droneForListId">the id for checking</param>
+        static void CheckExistenceOfDroneCharge(int droneForListId)
+        {
+            
+        }
+
+        /// <summary>
+        /// check if the id doesn't exist in the DronesChargeList
+        /// </summary>
+        /// <param name="droneForListId">the id for checking</param>
+        static void CheckNotExistenceOfDroneCharge(int droneForListId)
+        {
+           
+        }
+
+    }
+}
---
+++ dotNet5782_9627_5223/DalXml/DalXml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+using Singleton;
+
+namespace DalXml
+{
+    /// <summary>
+    ///the class DalXml contains all the needed methods 
+    ///which are connected to the data (in xml files) of the program.
+    /// </summary>
+    sealed partial class DalXml : Singleton<DalXml>, DalApi.IDal
+    {
+        // a constructor
+        DalXml()
+        {
+            //Initialize();
+        }
+    }
+}
