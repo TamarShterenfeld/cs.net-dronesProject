d3afdb152a1ea6826e5ba068511bb3436886002a
gilimaman@outlook.com <gilimaman@outlook.com>
2022-02-08 11:49:21 +0200

    the basic options of parcel entity work well.

--- dotNet5782_9627_5223/PL/AllParcelsTypes/Parcel/ParcelViewModel.cs
+++ dotNet5782_9627_5223/PL/AllParcelsTypes/Parcel/ParcelViewModel.cs
@@ -19,8 +19,7 @@ namespace PL
 {
     public class ParcelViewModel 
     {
-        BLApi.IBL bl;
-        public string State { get; set; }          
+        BLApi.IBL bl;     
         public bool EnableUpdate { get; set; }  
         public PO.Parcel Parcel { set; get; }       
         public Array PrioritiesArr { get; set; }
@@ -28,7 +27,6 @@ namespace PL
         public Array DroneStatusesList { get; set; }
         public RelayCommand Delete { get; set; }
         public RelayCommand LeftDoubleClick { get; set; }
-        public RelayCommand AddOrUpdate { get; set; }
         public RelayCommand Cancel { set; get; }
 
         public ParcelViewModel(BO.ParcelForList parcel, BLApi.IBL bl)
@@ -39,20 +37,14 @@ namespace PL
             WeightArr = typeof(BO.WeightCategories).GetEnumValues();
             DroneStatusesList = typeof(BO.DroneStatuses).GetEnumValues();
             Cancel = new(ButtonClick_Cancel);
-            AddOrUpdate = new(Button_ClickUpdate, null);
             Delete = new(Button_ClickDelete, null);
-            EnableUpdate = false;
-            State = "Update";
+            LeftDoubleClick = new(DoubleClick_Customer, null);
         }
 
         public ParcelViewModel(BLApi.IBL bl)
         {
             this.bl = bl;
             Parcel = new Parcel();
-            AddOrUpdate = new(Button_ClickAdd, null);
-            EnableUpdate = true;
-            State = "Add";
-            LeftDoubleClick = new(DoubleClick_Customer, null);
         }
 
         public void ButtonClick_Cancel(object sender)
@@ -71,23 +63,27 @@ namespace PL
                 MessageBox.Show("Can not delete this parcel since \nit has been associated already.");
                 return;
             }
+            if(Parcel == null)
+            {
+                MessageBox.Show("No parcel was chosen, \nnot possible deleting nothing.");
+            }
             bl.Delete(ParcelPoToBo(Parcel));
             ListsModel.Instance.DeleteParcel(Parcel.ParcelId);
             (sender as Window).Close();
         }
 
-        private void Button_ClickAdd(object sender)
-        {
-            bl.Add(ParcelPoToBo(Parcel));
-            ListsModel.Instance.AddParcel(Parcel.ParcelId);
-        }
-        private void Button_ClickUpdate(object sender)
-        {
-            bl.UpdateParcel(POConverter.ParcelPoToBo(Parcel));
-            ListsModel.Instance.UpdateParcel(Parcel.ParcelId);
-        }
-        
+        //private void Button_ClickAdd(object sender)
+        //{
+        //    bl.Add(ParcelPoToBo(Parcel));
+        //    ListsModel.Instance.AddParcel(Parcel.ParcelId);
+        //}
 
+        //there's no option to update something in parcel entity.
+        //private void Button_ClickUpdate(object sender)
+        //{
+        //    bl.UpdateParcel(ParcelPoToBo(Parcel));
+        //    ListsModel.Instance.UpdateParcel(Parcel.ParcelId);
+        //}
     }
 }
 
