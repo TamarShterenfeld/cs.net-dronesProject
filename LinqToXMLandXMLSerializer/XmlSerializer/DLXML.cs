318ef30a57db6db994a098c73a9bd093fb772df7
hitmachut <hitmachut@students.local>
2022-02-09 19:13:01 +0200

    contonuing working on XmlSerializer

---
+++ LinqToXMLandXMLSerializer/XmlSerializer/DLXML.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DS;
+
+namespace XmlSeralizerSample
+{
+    class DLXML : IDL
+    {
+        #region Student
+        
+        string studentFilePath = @"StudentsList.xml";
+        string courseFilePath = @"CoursesList.xml";
+        string studentInCourseFilePath = @"StudentInCourseList.xml";
+
+        public DLXML()
+        {
+            //if (!File.Exists(studentFilePath))
+            //    DL.XMLTools.SaveListToXMLSerializer<DO.Student>(DS.DataSource.studentList, studentFilePath);
+            
+            //if (!File.Exists(courseFilePath))
+            //    DL.XMLTools.SaveListToXMLSerializer<DO.Course>(DS.DataSource.courseList, courseFilePath);
+
+            //if (!File.Exists(studentInCourseFilePath))
+            //    DL.XMLTools.SaveListToXMLSerializer<DO.StudentInCourse>(DS.DataSource.studentInCourseList, studentInCourseFilePath);
+
+        }
+        public void AddStudent(DO.Student stu)
+        {
+            List<DO.Student> studentList = DL.XMLTools.LoadListFromXMLSerializer<DO.Student>(studentFilePath);
+
+            DO.Student stu1 = studentList.FirstOrDefault(t => t.StudentId == stu.StudentId);
+            
+            if (stu1 != null)
+            {
+                //throw new SomeException("DL: Student with the same id already exists...");
+            }
+            
+            studentList.Add(stu);
+            
+            DL.XMLTools.SaveListToXMLSerializer<DO.Student>(studentList, studentFilePath);
+        }
+        public void DeleteStudent(int id)
+        {
+            List<DO.Student> studentList = DL.XMLTools.LoadListFromXMLSerializer<DO.Student>(studentFilePath);
+
+            DO.Student stu1 = GetStudent(id);
+            
+            if (stu1 == null)
+            {
+                //throw new SomeException("DL: Student with the same id not found...");
+            }
+
+            List<DO.StudentInCourse> studentInCourseList = DL.XMLTools.LoadListFromXMLSerializer<DO.StudentInCourse>(studentInCourseFilePath);
+
+            if (studentInCourseList.Exists(s => s.StudentId == id))
+            {
+                //    throw new SomeException("DL: cannot delete student, courses for this student still exist !!!");
+            }
+
+            studentList.Remove(stu1);
+
+            DL.XMLTools.SaveListToXMLSerializer<DO.Student>(studentList, studentFilePath);            
+        }
+        public void UpdateStudent(DO.Student stu)
+        {
+            List<DO.Student> studentList = DL.XMLTools.LoadListFromXMLSerializer<DO.Student>(studentFilePath);
+
+            int index = studentList.FindIndex(t => t.StudentId == stu.StudentId);
+            
+            if (index == -1)
+                throw new Exception("DAL: Student with the same id not found...");
+
+            studentList[index] = stu;
+
+            DL.XMLTools.SaveListToXMLSerializer<DO.Student>(studentList, studentFilePath);
+        }
+        public DO.Student GetStudent(int id)
+        {
+            List<DO.Student> studentList = DL.XMLTools.LoadListFromXMLSerializer<DO.Student>(studentFilePath);
+            int index = studentList.FindIndex(t => t.StudentId == id);
+            if (index == -1)
+                throw new Exception("DAL: Student with the same id not found...");
+            return studentList.FirstOrDefault(t => t.StudentId == id);
+        }
+        public IEnumerable<DO.Student> GetAllStudents(Func<DO.Student, bool> predicat = null)
+        {
+            List<DO.Student> studentList = DL.XMLTools.LoadListFromXMLSerializer<DO.Student>(studentFilePath);
+            var v = from item in studentList
+                    select item; //item.Clone();
+
+            if (predicat == null)
+                return v.AsEnumerable().OrderByDescending(s => s.StudentId);
+
+            return v.Where(predicat).OrderByDescending(s => s.StudentId);
+        }
+
+
+        #endregion
+    }
+}
---
+++ LinqToXMLandXMLSerializer/XmlSerializer/DataSource.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS
+{
+    static class DataSource1
+    {
+        public static List<DO.Student> studentList;
+        public static List<DO.Course> courseList;
+        public static List<DO.StudentInCourse> studentInCourseList;
+
+        static DataSource1()
+        {
+            initLists();
+
+        }
+        private static void initLists()
+        {
+            courseList  = new List<DO.Course>();
+
+            string[] courseNameArray = new string[] { "c#", "java", "מבנה המחשב", "c++", "אינפי", "בדידה", "מבוא לתכנות" };
+            for (int i = 0; i < courseNameArray.Length; i++)
+                courseList.Add(new DO.Course { CourseId = i, CourseName = courseNameArray[i] });
+
+            string[] studentNameArray = new string[] { "אברהם", "יצחק", "יעקוב", "שרה", "רחל" };
+
+            int id = -1;
+
+            studentList = new List<DO.Student>
+            {
+                new DO.Student {StudentId = ++id ,
+                    IsMarried = true ,
+                    PersonDate = DateTime.ParseExact("24.03.85", "dd.MM.yy", null),
+                    PersonName = studentNameArray[id],
+                    StudentGender =  DO.Gender.male
+                },
+                new DO.Student {StudentId = ++id ,
+                    IsMarried = false ,
+                    PersonDate = DateTime.ParseExact("20.04.95", "dd.MM.yy", null),
+                    PersonName = studentNameArray[id],
+                    StudentGender = DO.Gender.male
+                },
+                new DO.Student {StudentId = ++id ,
+                    IsMarried = true ,
+                    PersonDate = DateTime.ParseExact("02.12.77", "dd.MM.yy", null),
+                    PersonName = studentNameArray[id],
+                    StudentGender = DO.Gender.male
+                },
+                new DO.Student {StudentId = ++id ,
+                    IsMarried = false ,
+                    PersonDate = DateTime.ParseExact("03.11.87", "dd.MM.yy", null),
+                    PersonName = studentNameArray[id],
+                    StudentGender = DO.Gender.female
+                },
+                new DO.Student {StudentId = ++id ,
+                    IsMarried = true ,
+                    PersonDate = DateTime.ParseExact("04.03.67", "dd.MM.yy", null),
+                    PersonName = studentNameArray[id],
+                    StudentGender = DO.Gender.female
+                },
+            };
+
+            studentInCourseList = new List<DO.StudentInCourse>();
+            
+            var rand = new Random();
+            for (int i = 0; i < studentList.Count; i++)
+                studentInCourseList.Add(new DO.StudentInCourse() { StudentId = i, CourseId = rand.Next(0, courseNameArray.Length), Grade = rand.Next(0,100) });
+        }
+    }
+}
