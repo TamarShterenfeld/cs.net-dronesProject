d5a1ba9c5d03c5e80604744bfffced4088845b04
gilimaman@outlook.com <gilimaman@outlook.com>
2022-02-14 12:42:24 +0200

    Validate & ValidateRules classes are constructed.

---
+++ dotNet5782_9627_5223/PL/HelpingClasses/Validation/ValidationRules.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PL
+{
+    class NameRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null) return new ValidationResult(false, "Required");
+            return Validation.IsValidName((string)value) ? ValidationResult.ValidResult : new ValidationResult(false, "Name is not valid");
+        }
+    }
+
+    class NumberRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value == null) return new ValidationResult(false, "Required");
+            return int.TryParse((string)value, out int num) ? ValidationResult.ValidResult : new ValidationResult(false, "Input has to contain a numerical value");
+        }
+    }
+
+    class NotEmptyRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            return value != null ?  ValidationResult.ValidResult : new ValidationResult(false, "Required");
+        }
+    }
+}
